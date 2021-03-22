be1202a4f093a52f8ccb9a98fe180dd04bf1e66d
rachitkakkar <71226802+rachitkakkar@users.noreply.github.com>
2021-03-22 08:48:03 -0500

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,15 @@
+import re
 
+data = [
+    ("^my name is", "Hello,")
+]
+
+# Boilerplate
+def respond_to(query, data):
+    for entry in data:
+        regex, replacement = entry
+
+        if re.match(regex, query,re.IGNORECASE):
+            print("found match")
+
+respond_to("My name is John Doe", data)
