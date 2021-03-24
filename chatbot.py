1e53b075cc2be419eddd3a6a3109755286d9b000
rachitkakkar <71226802+rachitkakkar@users.noreply.github.com>
2021-03-24 08:33:23 -0500

    Add Response Functionality + General Code Improvements
    I added the code to make a response if the jaccard index is greater than 0.5

--- chatbot.py
+++ chatbot.py
@@ -2,28 +2,23 @@ data = [
     ['how are you doing today', 'I am doing well!']
 ]
 
-
 def jaccard(a: set[str], b: set[str]) -> float:
     return float(len(a.intersection(b)))/len(a.union(b))
-
 
 def respond_to(query, data):
     query = query.lower()
 
-
-    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&;*_~'''
-    for mark in punctuations:  # Remove Punctuations
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&;*_~'''    
+    for mark in punctuations: # Remove Punctuations
         query = query.replace(mark, '')
 
     for entry in data:
         keywords, response = entry
-        keywords = keywords.split(' ')
-        response = response.split(' ')
-        query = query.split(' ')
+        
+        keywords = set(keywords.split(' '))
+        query = set(query.split(' '))
 
-        print(keywords, query, response)
+        if jaccard(query, keywords) > 0.5:
+            return response
 
-        return jaccard(query, keywords)
-
-
-print(respond_to('How are you doing today?', data))
+print(respond_to('How are you doing?', data))
