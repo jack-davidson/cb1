dd312fa334c87a25e5fd0748909eb036974601d4
rachitkakkar <71226802+rachitkakkar@users.noreply.github.com>
2021-03-24 08:22:41 -0500

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,28 @@
-import numpy as np
-
-a = [0, 1, 2, 5, 6, 8, 9]
-b = [0, 2, 3, 4, 5, 7, 9]
+data = [
+    ['how are you doing today', 'I am doing well!']
+]
 
 def jaccard(list1, list2):
     intersection = len(list(set(list1).intersection(list2)))
     union = (len(list1) + len(list2)) - intersection
     return float(intersection) / union
 
-jaccard(a, b)
+def respond_to(query, data):
+    query = query.lower()
+
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&;*_~'''    
+    for mark in punctuations: # Remove Punctuations
+        query = query.replace(mark, '')
+
+    for entry in data:
+        keywords, response = entry
+        
+        keywords = keywords.split(' ')
+        response = response.split(' ')
+        query = query.split(' ')
+
+        print(keywords, query, response)
+
+        return jaccard(query, keywords)
+
+print(respond_to('How are you doing today?', data))
