c7c53302a0cbee700a3c1d4148e3754865bc48be
jack-davidson <jackmaverickdavidson@gmail.com>
2021-03-24 08:26:10 -0500

    fixed spacing

--- chatbot.py
+++ chatbot.py
@@ -2,19 +2,21 @@ data = [
     ['how are you doing today', 'I am doing well!']
 ]
 
+
 def jaccard(a: set[str], b: set[str]) -> float:
     return float(len(a.intersection(b)))/len(a.union(b))
+
 
 def respond_to(query, data):
     query = query.lower()
 
-    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&;*_~'''    
-    for mark in punctuations: # Remove Punctuations
+
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&;*_~'''
+    for mark in punctuations:  # Remove Punctuations
         query = query.replace(mark, '')
 
     for entry in data:
         keywords, response = entry
-        
         keywords = keywords.split(' ')
         response = response.split(' ')
         query = query.split(' ')
@@ -22,5 +24,6 @@ def respond_to(query, data):
         print(keywords, query, response)
 
         return jaccard(query, keywords)
+
 
 print(respond_to('How are you doing today?', data))
