f1920da2f57adbef8424fd62873dffddcb64244a
Puneet Kakkar <rkcoding709@gmail.com>
2021-03-26 08:44:26 -0500

    IT WORKS

--- chatbot.py
+++ chatbot.py
@@ -1,24 +1,33 @@
-data = [
-    ['how are you doing today', 'I am doing well!']
-]
+import json
+
+with open("data/dataset.json") as f:
+    chat_data = json.loads(f.read())
+    f.close()
+
 
 def jaccard(a: set[str], b: set[str]) -> float:
     return float(len(a.intersection(b)))/len(a.union(b))
 
+
+def remove_punctuation(string):
+    punctuation = '''!()-[]{};:'"\\,<>./?@#$%^&;*_~'''
+    for mark in punctuation:  # Remove Punctuation
+        string = string.replace(mark, '')
+    return string
+
+
 def respond_to(query, data):
-    query = query.lower()
+    query = remove_punctuation(query.lower()).split()
 
-    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&;*_~'''    
-    for mark in punctuations: # Remove Punctuations
-        query = query.replace(mark, '')
+    suitable_entry = 0
+    for i in range(len(data)):
+        if jaccard(set(query), set(chat_data[i][0].split())) > jaccard(set(query), set(chat_data[suitable_entry][0].split())):
+            suitable_entry = i
 
-    for entry in data:
-        keywords, response = entry
-        
-        keywords = set(keywords.split(' '))
-        query = set(query.split(' '))
+    if jaccard(set(query), set(data[suitable_entry][0])) == 0:
+        return "I don't know"
 
-        if jaccard(query, keywords) > 0.5:
-            return response
+    return data[suitable_entry][1]
 
-print(respond_to('How are you doing?', data))
+
+print(respond_to('I dropped my phone in a lake', chat_data))
