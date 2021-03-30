142d159d064539e53203fff0c7d59511a6a376de
Puneet Kakkar <rkcoding709@gmail.com>
2021-03-30 08:08:40 -0500

    Added Logging v2

--- chatbot.py
+++ chatbot.py
@@ -4,20 +4,16 @@ with open("data/dataset.json") as f:
     chat_data = json.loads(f.read())
     f.close()
 
-
 def jaccard(a: set[str], b: set[str]) -> float:
     return float(len(a.intersection(b)))/len(a.union(b))
-
 
 def remove_punctuation(string):
     return string.strip('''!()-[]{};:'"\\,<>./?@#$%^&;*_~''')
 
-
 def write_string_to_file(string, filepath):
     file = open(filepath, 'a')
-    file.write(string)
+    file.write(string + '\n')
     file.close()
-
 
 def respond_to(query, data):
     if query == "quit":
@@ -25,7 +21,6 @@ def respond_to(query, data):
 
     cached_query = query # Save the query so we can write it to a text file it the bot can't understand it
     query = remove_punctuation(query.lower()).split()
-    print(query)
 
     best_response = 0
     for i in range(len(data)):
@@ -34,11 +29,12 @@ def respond_to(query, data):
             best_response = i
 
     if jaccard(set(query), set(data[best_response][0].split())) < 0.2:
-        print("ERROR")
+        write_string_to_file(str(query), "data/log.txt")
+        return "I cannot understand your problem. Transferring you to my superior."
 
     return data[best_response][1]
 
 
-print("type 'quit' to leave this conversation")
+print("Type 'quit' To Leave This Conversation")
 while True:
     print(respond_to(input("> "), chat_data))
