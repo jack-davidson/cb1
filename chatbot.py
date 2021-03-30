f1e446dea2bb329d40816280a940f40f6ba67924
jack-davidson <jackmaverickdavidson@gmail.com>
2021-03-30 07:44:40 -0500

    fixed bot

--- chatbot.py
+++ chatbot.py
@@ -4,16 +4,20 @@ with open("data/dataset.json") as f:
     chat_data = json.loads(f.read())
     f.close()
 
+
 def jaccard(a: set[str], b: set[str]) -> float:
     return float(len(a.intersection(b)))/len(a.union(b))
 
+
 def remove_punctuation(string):
     return string.strip('''!()-[]{};:'"\\,<>./?@#$%^&;*_~''')
+
 
 def write_string_to_file(string, filepath):
     file = open(filepath, 'a')
     file.write(string)
     file.close()
+
 
 def respond_to(query, data):
     if query == "quit":
@@ -21,20 +25,17 @@ def respond_to(query, data):
 
     cached_query = query # Save the query so we can write it to a text file it the bot can't understand it
     query = remove_punctuation(query.lower()).split()
+    print(query)
 
-    suitable_entry = 0
+    best_response = 0
     for i in range(len(data)):
-        keywords = chat_data[i][0].split()
-        if jaccard(set(query), set(keywords)) > jaccard(
-                set(query), set(keywords)):
-            suitable_entry = i
+        keyword = data[i][0].split()
+        if jaccard(set(query), set(keyword)) > jaccard(set(query), data[best_response][0].split()):
+            best_response = i
 
-    if jaccard(set(query), set(data[suitable_entry][0].split())) == 0:
-        write_string_to_file(cached_query, 'data/log.txt')
-        return "I cannot understand your problem. Transferring you to my superior."
-
-    return data[suitable_entry][1]
+    return data[best_response][1]
 
 
+print("type 'quit' to leave this conversation")
 while True:
     print(respond_to(input("> "), chat_data))
